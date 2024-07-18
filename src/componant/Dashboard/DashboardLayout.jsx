a70742e11716209d263da0fa2f076ba91484e361
imnazmul <nazmulislam2322@gmail.com>
2024-07-18 11:50:12 +0600

    feat: sendmoney

--- src/componant/Dashboard/DashboardLayout.jsx
+++ src/componant/Dashboard/DashboardLayout.jsx
@@ -5,15 +5,20 @@ import { AuthContexts } from "../../Context/AuthContext";
 const DashboardLayout = () => {
   const navigate = useNavigate();
   const { user } = useContext(AuthContexts);
-  const {name, balance} = user;
+  const { name, balance } = user;
 
   return (
     <div>
       <div className="max-w-screen-2xl mx-auto flex justify-between items-center my-10 text-xl">
         <p className="text-gray-800">হাই, {name}</p>
-        <button className="border px-4 py-2 rounded bg-gray-300">৳ {balance}</button>
+        <button className="border px-4 py-2 rounded bg-gray-300">
+          ৳ {balance}
+        </button>
       </div>
       <div className="max-w-screen-2xl mx-auto mb-4 *:py-2 *:px-4 *:bg-gray-500 flex flex-wrap gap-4 text-gray-200 *:rounded-md">
+        <button onClick={() => navigate("/dashboard")}>
+          ড্যাশবোর্ড
+        </button>
         <button onClick={() => navigate("/dashboard/sendmoney")}>
           সেন্ড মানি
         </button>
--- src/componant/Sendmoney/Sendmoney.jsx
+++ src/componant/Sendmoney/Sendmoney.jsx
@@ -1,12 +1,59 @@
-
+import { useContext, useState } from "react";
+import { AuthContexts } from "../../Context/AuthContext";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const Sendmoney = () => {
-    return (
-        <div className="max-w-screen-2xl mx-auto mt-10">
-            
-            
+  const { user } = useContext(AuthContexts);
+  const [amount, setAmount] = useState(0);
+  const axiosSecure = useAxiosSecure();
+
+  const handleSendmoney = async (e) => {
+    e.preventDefault();
+    if (user.balance < amount) {
+      toast.error("Insufficiant balance");
+    }
+
+    try {
+      const res = await axiosSecure.post("/sendmoney", {
+        amount,
+        receiver: e.target.phone.value,
+      });
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.error(error.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="max-w-screen-2xl mx-auto mt-10 border p-20">
+      <form onSubmit={handleSendmoney} className="text-xl">
+        <div className="mb-2">
+          <p className="mb-1">Recivers Number:</p>
+          <input
+            className="outline-none rounded-md px-2 py-1 bg-gray-200"
+            type="number"
+            name="phone"
+            required
+          />
         </div>
-    );
+        <div className="mb-2">
+          <p className="mb-1">Amount:</p>
+          <input
+            onChange={(e) => setAmount(e.target.value)}
+            className="outline-none rounded-md px-2 py-1 bg-gray-200"
+            type="number"
+            name="amount"
+            required
+          />
+        </div>
+        <p>Estemeted Fee: {amount > 100 ? 5 : 0}</p>
+        <button className="py-2 px-4 bg-blue-500 rounded text-gray-200 mt-2 hover:bg-blue-400 duration-300">
+          Send Money
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Sendmoney;
